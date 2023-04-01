c3e499a2b0bea4d26578c6a94ecb930c4cb01913
harutoreon <norabder567@gmail.com>
2023-04-02 08:58:55 +0900

    Finished Theme

--- operation-of-variable.js
+++ operation-of-variable.js
@@ -1,11 +1,7 @@
-<!DOCTYPE HTML>
-<html>
-  <head>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <script>
-      
-    </script>
-  </body>
-</html>
+let admin;
+let name;
+
+name  = "John";
+admin = name;
+
+alert(`${admin}`);
