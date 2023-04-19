786eda196d588a5acf5bf513ae1a65172a2091ea
harutoreon <norabder567@gmail.com>
2023-04-19 10:26:55 +0900

    add: Implementation complete of sum function

---
+++ mocha/function_sum_spec.js
@@ -0,0 +1,11 @@
+describe("sum", function() {
+
+  it("Does the total value equal 4?", function() {
+    assert.equal(sum(3, 1), 4);
+  });
+
+  it("Does the total value equal 8?", function() {
+    assert.equal(sum(5, 3), 8);
+  });
+
+});
