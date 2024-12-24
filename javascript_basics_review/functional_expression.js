6096b342a1d564dad3170de26370bca27c9811f8
harutoreon <norabder567@gmail.com>
2024-12-24 23:58:50 +0900

    Add function_expression.js

---
+++ javascript_basics_review/functional_expression.js
@@ -0,0 +1,12 @@
+function ask(question, yes, no) {
+  if (confirm(question)) {
+    yes();
+  } else {
+    no();
+  }
+}
+
+ask("Do you agree?",
+  function() { console.log( 'You agreed.' ); },
+  function() { console.log( 'You canceled the execution.' ); }
+);
