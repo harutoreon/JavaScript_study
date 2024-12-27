f358086b0cd3c9065a691bbb29af1e586c29d592
harutoreon <norabder567@gmail.com>
2024-12-27 20:37:42 +0900

    Change promise code to microtasks

--- javascript_basics_review/promise.js
+++ javascript_basics_review/promise.js
@@ -1,16 +1,3 @@
-let cache = new Map();
-
-function loadCached(url) {
-  if (cache.has(url)) {
-    return Promise.resolve(cache.get(url));  // (*)
-  }
-
-  return fetch(url)
-    .then(response => response.text())
-    .then(text => {
-      cache.set(url,text);
-      return text;
-    });
-}
-
-console.log(loadCached('https://api.github.com/users/iliakan'));
+Promise.resolve()
+  .then(() => console.log("promise done!"))
+  .then(() => console.log("code finished"));
